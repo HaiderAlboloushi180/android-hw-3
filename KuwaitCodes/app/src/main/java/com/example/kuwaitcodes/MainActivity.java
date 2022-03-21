7a5e7238aa5408b4558499b668ba3d660f0d7525
HaiderAlboloushi180 <halbolou05@gmail.com>
2022-03-21 16:59:22 +0300

    edited

--- KuwaitCodes/app/src/main/java/com/example/kuwaitcodes/MainActivity.java
+++ KuwaitCodes/app/src/main/java/com/example/kuwaitcodes/MainActivity.java
@@ -14,16 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.countinueButton);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-
-                startActivity(intent);
-            }
-        });
 
     }
 }
--- KuwaitCodes/app/src/main/java/com/example/kuwaitcodes/MainActivity3.java
+++ KuwaitCodes/app/src/main/java/com/example/kuwaitcodes/MainActivity3.java
@@ -15,29 +15,5 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
-        ImageView referenceImageView = findViewById(R.id.referenceImageView2);
-        TextView titleTextView = findViewById(R.id.titleTextView2);
-        TextView descriptionTextView = findViewById(R.id.descriptionTextView2);
-        Button button = findViewById( R.id.button1);
-
-        Bundle bundle = getIntent().getExtras();
-
-        int imageResource = bundle.getInt("imageResource");
-        String description = bundle.getString("description");
-        String title = bundle.getString("title");
-
-        referenceImageView.setImageResource(imageResource);
-        descriptionTextView.setText(description);
-        titleTextView.setText(title);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity3.this,  "تم التسجيل" , Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
     }
 }
